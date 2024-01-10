833a8bc233f94ce3f9463e2ce6559a294791ce4d
albayerga <114679687+albayerga@users.noreply.github.com>
2024-01-10 10:57:18 +0100

    Add files via upload

---
+++ lib1.h
@@ -0,0 +1,13 @@
+/*
+ * Compilers
+ * Practice 1: c pre-processor
+ * File name: comp-p1.c
+ * Main program file
+ * Responsible: put the names of people responsible of the modifications of this file
+ */
+#include "./comp-p1.h"
+
+typedef struct{
+    int field1;
+    int field2;
+} stype;
