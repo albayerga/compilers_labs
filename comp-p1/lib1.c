9dbff49db1b33b7478f71fe2508f041ed9a7fc80
albayerga <alba.yerga01@estudiant.upf.edu>
2024-01-10 10:50:23 +0100

    subir archivos

---
+++ comp-p1/lib1.c
@@ -0,0 +1,56 @@
+/*
+ * Compilers
+ * Practice 1: c pre-processor
+ * File name: lib1.h
+ * Functionalities of the library 1 (modify comment to be explicit
+ * on what functionalities lib1 provides)
+ * 
+ * Define as many block functionalities (lib's) as the problem 
+ * requires and also to assign a different block design and 
+ * implementation to each person in the team
+ * 
+ * (Modify comments based on what you need this code to do)
+ * Data structures and definitions defined by the lib1
+ * 
+ * Responsible: put the names of people responsible of the modifications of this file
+ */
+#include "./comp-p1.h"
+#include "./lib1.h"
+
+stype var;
+
+void func1() {
+    
+
+    printf("hola que tal com vas? estic a func1\n");
+/*
+    ofile = stdout;             // default is stdout
+    ofile = fopen( PRINTDESTFILE, "w");
+    if(ofile == NULL)                 
+        WARNING(0, (ofile, "Problem creating %s", PRINTDESTFILE));
+
+    errfile = stdout;           // default is stdout
+    errfile = fopen( PRINTERRORFILE, "w");
+    if(errfile != NULL)                
+        WARNING(0, (ofile, "Problem creating %s", PRINTERRORFILE));
+    
+
+    // Prints arguments
+    fprintf(ofile, "Arguments:\n");
+    for (i = 0; i < argc; i++) {
+        fprintf(ofile, "%i: %s\n", i, argv[i]);
+#if (PRINTWARNING == ON)
+        WARNING(i, (errfile, "warning print example %s \n", argv[i]));
+#endif
+    }
+    
+#if (PRINTERROR == ON)
+    ERROR(i, (errfile, "This is an error, and it stops"));
+#endif
+    
+    fclose(ofile);
+    fclose(errfile);
+
+    return 0; 
+ */  
+}
