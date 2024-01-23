c5f56d58d447c146ef46b9b8ddbc528a1d779ce2
albayerga <alba.yerga01@estudiant.upf.edu>
2024-01-23 14:38:13 +0100

    remove_comments

--- comp-p1/preprocessor.c
+++ comp-p1/preprocessor.c
@@ -12,6 +12,50 @@ void printManPage()
     printf("  -help   Print this man page\n");
 }
 
+int removeComments(const char *input_file_name)
+{
+    char output_file_name[100];
+    snprintf(output_file_name, sizeof(output_file_name), "%s_pp.c", input_file_name);
+
+    FILE *input = fopen(input_file_name, "r");
+    FILE *output = fopen(output_file_name, "w");
+
+    if (input == NULL || output == NULL) {
+        perror("Error opening files");
+        return 1; // Return an error code
+    }
+
+    // Code to read input file, remove comments, and write to output file
+    int c;
+    int last = 0;
+    while ((c = fgetc(input)) != EOF)
+    {
+        if (c == '/' && last == '/')
+        {
+            while ((c = fgetc(input)) != EOF && c != '\n');
+            last = 0;
+        }
+        else if (c == '*' && last == '/')
+        {
+            while ((c = fgetc(input)) != EOF && (c != '/' || last != '*'))
+            {
+                last = c;
+            }
+            last = 0;
+        }
+        else
+        {
+            fputc(last, output);
+            last = c;
+        }
+    }
+
+    fclose(input);
+    fclose(output);
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc < 2)
@@ -30,8 +74,16 @@ int main(int argc, char *argv[])
     //-c
     if (argc >= 2 && strcmp(argv[1], "-c") == 0)
     {
-        printf("This will eliminate comments\n");
-        return 0;
+        if(argc != 3)
+        {
+            printf("Usage: preprocessor -c <program.c>\n");
+            return 0;
+        }
+        else
+        {
+            const char *filename = argv[2];
+            removeComments(filename);
+        }
     }
 
     //-d
